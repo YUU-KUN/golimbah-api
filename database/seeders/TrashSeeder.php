cead79ca84943b522e28184dd015daff414c7aea
Indra <iwahyu@student.telkomuniversity.ac.id>
2023-12-11 20:18:07 +0700

    [Feat] Updated Trash Seeder

--- database/seeders/TrashSeeder.php
+++ database/seeders/TrashSeeder.php
@@ -15,12 +15,105 @@ class TrashSeeder extends Seeder
      */
     public function run()
     {
-        Trash::create([
-            'name' => 'Sisa Makanan',
-            'description' => 'Didapatkan dari sisa-sisa rumah tangga. Ini adalah sampah organik yang bisa diuraikan.',
-            'category' => 'organic',
-            'photo' => 'sisa_makanan.jpg',
-            'game_mode' => 'easy'
-        ]);
+
+        $trashes = [
+            [
+                'name' => 'Kaleng',
+                'description' => '',
+                'category' => 'inorganic',
+                'photo' => '1698938101.png',
+                'game_mode' => 'easy'
+            ],
+            [
+                'name' => 'Kulit Pisang',
+                'description' => '',
+                'category' => 'organic',
+                'photo' => '1698905261.png',
+                'game_mode' => 'easy'
+            ],
+            [
+                'name' => 'Botol Kaca',
+                'description' => '',
+                'category' => 'glass',
+                'photo' => '1698938522.png',
+                'game_mode' => 'hard'
+            ],
+            [
+                'name' => 'Bungkus Snack',
+                'description' => '',
+                'category' => 'inorganic',
+                'photo' => '1698938455.png',
+                'game_mode' => 'hard'
+            ],
+            [
+                'name' => 'Lampu',
+                'description' => '',
+                'category' => 'glass',
+                'photo' => '1698938544.png',
+                'game_mode' => 'hard'
+            ],
+            [
+                'name' => 'Lampu',
+                'description' => '',
+                'category' => 'inorganic',
+                'photo' => '1698938330.png',
+                'game_mode' => 'easy'
+            ],
+            [
+                'name' => 'Sisa Makanan',
+                'description' => 'Didapatkan dari sisa-sisa rumah tangga. Ini adalah sampah organik yang bisa diuraikan',
+                'category' => 'organic',
+                'photo' => '1698937839.png',
+                'game_mode' => 'easy'
+            ],
+            [
+                'name' => 'Kulit Apel',
+                'description' => '',
+                'category' => 'organic',
+                'photo' => '1698937891.png',
+                'game_mode' => 'easy'
+            ],
+            [
+                'name' => 'Kantong Kresek',
+                'description' => '',
+                'category' => 'inorganic',
+                'photo' => '1698938438.png',
+                'game_mode' => 'hard'
+            ],
+            [
+                'name' => 'Botol Kaca',
+                'description' => '',
+                'category' => 'inorganic',
+                'photo' => '1698938310.png',
+                'game_mode' => 'easy'
+            ],
+            [
+                'name' => 'Kertas',
+                'description' => '',
+                'category' => 'inorganic',
+                'photo' => '1698938390.png',
+                'game_mode' => 'easy'
+            ],
+            [
+                'name' => 'Kertas',
+                'description' => '',
+                'category' => 'paper',
+                'photo' => '1698938570.png',
+                'game_mode' => 'hard'
+            ],
+            [
+                'name' => 'Kaleng',
+                'description' => '',
+                'category' => 'inorganic',
+                'photo' => '1698938115.png',
+                'game_mode' => 'easy'
+            ],
+
+        ];
+
+        foreach ($trashes as $trash) {
+            Trash::create($trash);
+        }
+
     }
 }
